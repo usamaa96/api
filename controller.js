a689f2d0bbed887a29f901eb4736f8ae662973ec
usama.abubakar <usama.abubakar@binaryvibes.com>
2019-10-18 16:05:39 +0500

    Updated

--- controller.js
+++ controller.js
@@ -1,15 +1,5 @@
 var app = angular.module('myApp', []);
 
+app.controller('myCtrl', function($scope, myService) {
 
-app.controller('myCtrl', function($scope, $http, myService) {
-
-	$scope.resetData = function(){
-		myService.resetData()
-		.then(value => {
-			$scope.value = value;
-		})
-		.catch(err => {
-			console.log(err);
-		});
-	}
 });
--- service.js
+++ service.js
@@ -2,20 +2,18 @@ angular.module('myApp').factory('myService', MyService);
 
 function MyService($http){
     return {
-        resetData: resetData
+        getAllData: getAllData
     }
 
-    function resetData(){
+    function getAllData(){
         return $http({
-            url: "http://localhost:3000/resetData",
+            url: "http://192.168.0.42:9000/api/getAllStudents",
             method: "GET",
-            contentType: 'application/json',
-        }).
-        then(response => {
-            return response.data;
-        })
-        .catch(error => {
-            return response.data;
-        })
+            contentType: "application/json"
+        }).then(response => {
+            return response;
+        }).catch(error => {
+            console.log(error);
+        });
     }
 }
